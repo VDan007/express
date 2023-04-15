c5cd7b4c2d3afb8b5d44ff240a127ad9d0bab2fa
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-15 15:58:16 +0200

    express started

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.listen(3000);
+
+app.get('/',(req,res)=>{
+   // res.send('<p>Home page</p>');
+   res.sendFile('./views/index.html',{ root:__dirname});
+});
+app.get('/about',(req,res)=>{
+   // res.send('<p>About page</p>');
+   res.sendFile('./views/about.html',{ root:__dirname });
+});
+
+app.get('/about-us',(req,res)=>{
+    res.redirect('./about');
+});
